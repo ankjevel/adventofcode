9ff34fa54af0c5b35c4de251955313a43233d43d
Dennis Pettersson <mail@dennispettersson.se>
2020-11-16 08:58:27 +0100

    feat: rewrote part 2

--- day08/src/part_02.rs
+++ day08/src/part_02.rs
@@ -1,63 +1,58 @@
 use crate::parse_input;
-use std::{io::Result, sync::Mutex};
+use std::{io::Result, str};
 
-struct Entry {
-    index: usize,
-    children: Vec<usize>,
-    entries: Vec<u128>,
+struct Node {
+    size: usize,
+    children: Vec<Node>,
+    metadata: Vec<u128>,
 }
 
-struct Query<'a> {
-    entries: &'a mut Vec<u128>,
-    metadata: &'a mut Vec<Entry>,
-}
+impl Node {
+    fn new(input: &[u128]) -> Option<Node> {
+        if input.len() < 2 {
+            return None;
+        }
 
-fn query_children<F>(query: Query, mut next: F) -> usize
-where
-    F: FnMut() -> usize + Copy,
-{
-    let number_of_child_nodes = query.entries.remove(0);
-    let number_of_metadata_entries = query.entries.remove(0);
-    let index = next();
+        let child_nodes = input[0];
+        let metadata_entres = input[1];
 
-    let mut children = Vec::new();
-    for _ in 0..number_of_child_nodes {
-        children.push(query_children(
-            Query {
-                entries: query.entries,
-                metadata: query.metadata,
-            },
-            next,
-        ));
+        let mut node = Node {
+            size: 2,
+            children: Vec::new(),
+            metadata: Vec::new(),
+        };
+
+        for _ in 0..child_nodes {
+            if let Some(child) = Node::new(&input[node.size..]) {
+                node.size += child.size;
+                node.children.push(child);
+            }
+        }
+
+        for _ in 0..metadata_entres {
+            if let Some(&meta) = input.get(node.size) {
+                if meta > 0 {
+                    node.metadata.push(meta);
+                    node.size += 1;
+                }
+            }
+        }
+
+        Some(node)
     }
 
-    let mut entries = Vec::new();
-    for _ in 0..number_of_metadata_entries {
-        entries.push(query.entries.remove(0));
-    }
+    fn value(&self) -> u128 {
+        if self.children.is_empty() {
+            return self.metadata.iter().sum();
+        }
 
-    query.metadata.push(Entry {
-        index,
-        children,
-        entries,
-    });
-
-    query.metadata.len()
-}
-
-fn sum_of_entries(index: usize, entry: &Entry, entries: &Vec<Entry>) -> u128 {
-    let current_index = entry.index;
-    let index_to_find = current_index + index as usize;
-
-    if index_to_find >= entries.len() {
-        return 0;
-    }
-
-    let child = entries.get(index_to_find).unwrap();
-    if child.children.len() > 0 {
-        sum_of_entries(child.index as usize, child, entries)
-    } else {
-        child.entries.clone().into_iter().sum()
+        self.metadata.iter().fold(0, |sum, index| {
+            if let Some(child) = self.children.get(*index as usize - 1) {
+                sum + child.value()
+            } else {
+                sum
+            }
+        })
     }
 }
 
@@ -66,26 +61,7 @@ pub fn main(input: &str) -> Result<u128> {
         .into_iter()
         .map(|e| e.into())
         .collect::<Vec<u128>>();
-    let mut metadata = Vec::new();
-    let current_index = &Mutex::new(0);
-    query_children(
-        Query {
-            entries: &mut entries.clone(),
-            metadata: &mut metadata,
-        },
-        move || {
-            let mut index = current_index.lock().unwrap();
-            *index += 1;
-            index.clone() - 1
-        },
-    );
-
-    metadata.sort_by(|a, b| a.index.cmp(&b.index));
-
-    let first = metadata.first().unwrap();
-    Ok(first.entries.iter().fold(0, |acc, index| {
-        acc + sum_of_entries(index.to_owned() as usize, &first, &metadata)
-    }))
+    Ok(Node::new(&entries).unwrap().value())
 }
 
 #[cfg(test)]
