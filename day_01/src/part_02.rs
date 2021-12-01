caa6e6aa357f5d07926ccfb09f168178c773945c
Dennis Pettersson <mail@dennispettersson.se>
2021-12-01 17:25:18 +0100

    feat(day_01): completed part 2

--- day_01/src/part_02.rs
+++ day_01/src/part_02.rs
@@ -2,8 +2,27 @@ use std::io::Result;
 
 use crate::Input;
 
-pub fn main(_input: &Input) -> Result<u32> {
-    Ok(0)
+pub fn main(input: &Input) -> Result<u32> {
+    let mut last = None;
+    let mut measurement_increases = 0;
+    for value in input.windows(3) {
+        let window = value
+            .into_iter()
+            .map(u32::to_owned)
+            .reduce(|a, b| a + b)
+            .unwrap_or(0);
+
+        match last {
+            Some(pre_value) => {
+                if pre_value < window {
+                    measurement_increases += 1;
+                }
+            }
+            _ => {}
+        }
+        last = Some(window.to_owned());
+    }
+    Ok(measurement_increases)
 }
 
 #[cfg(test)]
